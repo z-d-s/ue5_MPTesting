b0379a4ec6cf3dd8c7d4f5a2f81ea3329be31cfc
zhangdiansong <zhangdiansong@thedream.cc>
2022-07-28 10:33:34 +0800

    添加online subsystem steam plugin

--- Source/MPTesting/MPTesting.Build.cs
+++ Source/MPTesting/MPTesting.Build.cs
@@ -8,6 +8,6 @@ public class MPTesting : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "OnlineSubsystemSteam", "OnlineSubsystem" });
 	}
 }
